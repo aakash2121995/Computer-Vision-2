7f75123d67d38828f4db0fba6e25e4b845f119d6
Aakash Aggarwal <aa@cp-bap.de>
2020-05-02 20:14:24 +0200

    Adding Sheet-01 with Regression Solution

---
+++ Sheet01/sheet01.py
@@ -0,0 +1,150 @@
+import numpy as np
+from numpy.linalg import inv
+
+##############################################################################################################
+#Auxiliary functions for Regression
+##############################################################################################################
+#returns features with bias X (num_samples*(1+num_features)) and target values Y (num_samples*target_dims)
+def read_data_reg(filename):
+    data = np.loadtxt(filename)
+    Y = data[:,:2]
+    X = np.concatenate((np.ones((data.shape[0], 1)), data[:,2:]), axis=1)
+    return Y, X
+
+#takes features with bias X (num_samples*(1+num_features)) and target values Y (num_samples*target_dims)
+#returns regression coefficients w ((1+num_features)*target_dims)
+def lin_reg(X, Y):
+    return inv(X@X.T)@X@Y
+
+#takes features with bias X (num_samples*(1+num_features)), target Y (num_samples*target_dims) and regression coefficients w ((1+num_features)*target_dims)
+#returns fraction of mean square error and variance of target prediction separately for each target dimension
+def test_lin_reg(X, Y, w):
+    Y_pred = X@w
+    mse = np.mean(np.square(Y_pred - Y))
+    return mse/np.var(Y)
+
+#takes features with bias X (num_samples*(1+num_features)), centers of clusters C (num_clusters*(1+num_features)) and std of RBF sigma
+#returns matrix with scalar product values of features and cluster centers in higher embedding space (num_samples*num_clusters)
+def RBF_embed(X, C, sigma):
+    embedding = np.zeros((X.shape[0],C.shape[0]))
+    for center_id in range(C.shape[0]):
+        center = C[center_id]
+        embedding[:, center_id] = np.exp(-np.sum((X - center) ** 2, axis=1) / sigma)
+    return embedding
+############################################################################################################
+#Linear Regression
+############################################################################################################
+
+def run_lin_reg(X_tr, Y_tr, X_te, Y_te):
+
+    print('MSE/Var linear regression')
+    w = lin_reg(X_tr.T,Y_tr)
+    err = test_lin_reg(X_te,Y_te,w)
+    print(err)
+
+############################################################################################################
+#Dual Regression
+############################################################################################################
+def run_dual_reg(X_tr, Y_tr, X_te, Y_te, tr_list, val_list):
+    for sigma_pow in range(-5, 3):
+        sigma = np.power(3.0, sigma_pow)
+        print('MSE/Var dual regression for val sigma='+str(sigma))
+        print(err_dual)
+
+    print('MSE/Var dual regression for test sigma='+str(opt_sigma))
+    print(err_dual)
+
+############################################################################################################
+#Non Linear Regression
+############################################################################################################
+def run_non_lin_reg(X_tr, Y_tr, X_te, Y_te, tr_list, val_list):
+    from sklearn.cluster import KMeans
+    best_error = 10**5
+    opt_sigma, opt_num_clusters = 0, 0
+    for num_clusters in [10, 30, 100]:
+        for sigma_pow in range(-5, 3):
+            sigma = np.power(3.0, sigma_pow)
+            X_train = X_tr[tr_list]
+            Y_train = Y_tr[tr_list]
+            Y_val = Y_tr[val_list]
+            kmeans = KMeans(num_clusters)
+            kmeans.fit(X_train)
+            clusters = kmeans.cluster_centers_
+            Z_train = RBF_embed(X_train,clusters,sigma)
+            w = lin_reg(Z_train.T, Y_train)
+            Z_val  = RBF_embed(X_tr[val_list],clusters,sigma)
+            err_dual = test_lin_reg(Z_val, Y_val, w)
+            if err_dual < best_error:
+                best_error = err_dual
+                opt_sigma = sigma
+                opt_num_clusters = num_clusters
+            print('MSE/Var non linear regression for val sigma='+str(sigma)+' val num_clusters='+str(num_clusters))
+            print(err_dual)
+
+    print('MSE/Var non linear regression for test sigma='+str(opt_sigma)+' test num_clusters='+str(opt_num_clusters))
+    print(err_dual)
+
+####################################################################################################################################
+#Auxiliary functions for classification
+####################################################################################################################################
+#returns features with bias X (num_samples*(1+num_feat)) and gt Y (num_samples)
+def read_data_cls(split):
+    feat = {}
+    gt = {}
+    for category in [('bottle', 1), ('horse', -1)]: 
+        feat[category[0]] = np.loadtxt('data/'+category[0]+'_'+split+'.txt')
+        feat[category[0]] = np.concatenate((np.ones((feat[category[0]].shape[0], 1)), feat[category[0]]), axis=1)
+        gt[category[0]] = category[1] * np.ones(feat[category[0]].shape[0])
+    X = np.concatenate((feat['bottle'], feat['horse']), axis=0)
+    Y = np.concatenate((gt['bottle'], gt['horse']), axis=0)
+    return Y, X
+
+# takes features with bias X (num_samples*(1+num_features)), gt Y (num_samples) and current_parameters w (num_features+1)
+# Y must be from {-1, 1}
+# returns gradient with respect to w (num_features)
+def log_llkhd_grad(X, Y, w):
+    pass
+
+#takes features with bias X (num_samples*(1+num_features)), gt Y (num_samples) and current_parameters w (num_features+1)
+# Y must be from {-1, 1}
+#returns log likelihood loss
+def get_loss(X, Y, w):
+    pass
+
+#takes features with bias X (num_samples*(1+num_features)), gt Y (num_samples) and current_parameters w (num_features+1)
+# Y must be from {-1, 1}
+#returns accuracy
+def get_accuracy(X, Y, w):
+    pass
+
+####################################################################################################################################
+#Classification
+####################################################################################################################################
+def run_classification(X_tr, Y_tr, X_te, Y_te, step_size):
+    print('classification with step size '+str(step_size))
+    max_iter = 10000
+    for step in range(max_iter):
+        if step%1000 == 0:
+            print('step='+str(step)+' loss='+str(loss)+' accuracy='+str(accuracy))
+
+    print('test set loss='+str(loss)+' accuracy='+str(accuracy))
+
+
+####################################################################################################################################
+#Exercises
+####################################################################################################################################
+Y_tr, X_tr = read_data_reg('data/regression_train.txt')
+Y_te, X_te = read_data_reg('data/regression_test.txt')
+
+run_lin_reg(X_tr, Y_tr, X_te, Y_te)
+
+tr_list = list(range(0, int(X_tr.shape[0]/2)))
+val_list = list(range(int(X_tr.shape[0]/2), X_tr.shape[0]))
+
+# run_dual_reg(X_tr, Y_tr, X_te, Y_te, tr_list, val_list)
+run_non_lin_reg(X_tr, Y_tr, X_te, Y_te, tr_list, val_list)
+
+step_size = 0.0001
+Y_tr, X_tr = read_data_cls('test')
+Y_te, X_te = read_data_cls('test')
+run_classification(X_tr, Y_tr, X_te, Y_te, step_size)
