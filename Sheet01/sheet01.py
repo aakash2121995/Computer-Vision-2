7e5bc5616659ff3d49bfa3d3fcc4e5f9cb69c29b
Aakash Aggarwal <aa@cp-bap.de>
2020-05-04 13:47:03 +0200

    Adding Solution to Regression Problem

--- Sheet01/sheet01.py
+++ Sheet01/sheet01.py
@@ -20,8 +20,7 @@ def lin_reg(X, Y):
 #returns fraction of mean square error and variance of target prediction separately for each target dimension
 def test_lin_reg(X, Y, w):
     Y_pred = X@w
-    mse = np.mean(np.square(Y_pred - Y))
-    return mse/np.var(Y)
+    return np.mean(np.square(Y - Y_pred),axis=0)/np.var(Y,axis=0)
 
 #takes features with bias X (num_samples*(1+num_features)), centers of clusters C (num_clusters*(1+num_features)) and std of RBF sigma
 #returns matrix with scalar product values of features and cluster centers in higher embedding space (num_samples*num_clusters)
@@ -29,7 +28,7 @@ def RBF_embed(X, C, sigma):
     embedding = np.zeros((X.shape[0],C.shape[0]))
     for center_id in range(C.shape[0]):
         center = C[center_id]
-        embedding[:, center_id] = np.exp(-np.sum((X - center) ** 2, axis=1) / sigma)
+        embedding[:, center_id] = np.exp(-np.sum((X - center) ** 2, axis=1) / (2*sigma**2))
     return embedding
 ############################################################################################################
 #Linear Regression
@@ -46,11 +45,39 @@ def run_lin_reg(X_tr, Y_tr, X_te, Y_te):
 #Dual Regression
 ############################################################################################################
 def run_dual_reg(X_tr, Y_tr, X_te, Y_te, tr_list, val_list):
+    # Parameters for Best Model
+    best_error = 10 ** 5 * np.ones((2,))
+    opt_sigma = 0
+
+    # Train Validation Split
+    X_train = X_tr[tr_list]
+    Y_train = Y_tr[tr_list]
+    Y_val = Y_tr[val_list]
+    X_val = X_tr[val_list]
+
     for sigma_pow in range(-5, 3):
         sigma = np.power(3.0, sigma_pow)
+
+        # Kernel Embeddings and regression
+        X_T_X_ = RBF_embed(X_train,X_train,sigma)
+        w = lin_reg(X_T_X_, Y_train)
+        X_T_X_val = RBF_embed(X_val,X_train, sigma)
+        err_dual = test_lin_reg(X_T_X_val, Y_val, w)
+
+        # Validation Error
         print('MSE/Var dual regression for val sigma='+str(sigma))
         print(err_dual)
+        if all(err_dual - best_error < 0):
+            opt_sigma = sigma
+            best_error = err_dual
 
+    # Recomputation with the best sigma
+    X_T_X_ = RBF_embed(X_train, X_train, opt_sigma)
+    w = lin_reg(X_T_X_, Y_train)
+
+    # Test Error Computation
+    X_T_X_test = RBF_embed(X_te, X_train, opt_sigma)
+    err_dual = test_lin_reg(X_T_X_test, Y_te, w)
     print('MSE/Var dual regression for test sigma='+str(opt_sigma))
     print(err_dual)
 
@@ -59,29 +86,43 @@ def run_dual_reg(X_tr, Y_tr, X_te, Y_te, tr_list, val_list):
 ############################################################################################################
 def run_non_lin_reg(X_tr, Y_tr, X_te, Y_te, tr_list, val_list):
     from sklearn.cluster import KMeans
-    best_error = 10**5
+    best_error = 10**5*np.ones((2,))
     opt_sigma, opt_num_clusters = 0, 0
+    X_train = X_tr[tr_list]
+    Y_train = Y_tr[tr_list]
+    Y_val = Y_tr[val_list]
+    X_val = X_tr[val_list]
     for num_clusters in [10, 30, 100]:
+        # Running Kmeans
+        kmeans = KMeans(num_clusters)
+        kmeans.fit(X_train)
+        clusters = kmeans.cluster_centers_
         for sigma_pow in range(-5, 3):
             sigma = np.power(3.0, sigma_pow)
-            X_train = X_tr[tr_list]
-            Y_train = Y_tr[tr_list]
-            Y_val = Y_tr[val_list]
-            kmeans = KMeans(num_clusters)
-            kmeans.fit(X_train)
-            clusters = kmeans.cluster_centers_
+
+            # RBF Kernel application for Regression
             Z_train = RBF_embed(X_train,clusters,sigma)
             w = lin_reg(Z_train.T, Y_train)
-            Z_val  = RBF_embed(X_tr[val_list],clusters,sigma)
+            Z_val  = RBF_embed(X_val,clusters,sigma)
             err_dual = test_lin_reg(Z_val, Y_val, w)
-            if err_dual < best_error:
+
+            # Saving Optimal Parameters
+            if  all(err_dual - best_error < 0):
                 best_error = err_dual
                 opt_sigma = sigma
                 opt_num_clusters = num_clusters
             print('MSE/Var non linear regression for val sigma='+str(sigma)+' val num_clusters='+str(num_clusters))
             print(err_dual)
 
+    # Recomputation with best parameters
     print('MSE/Var non linear regression for test sigma='+str(opt_sigma)+' test num_clusters='+str(opt_num_clusters))
+    kmeans = KMeans(opt_num_clusters)
+    kmeans.fit(X_train)
+    clusters = kmeans.cluster_centers_
+    Z_train = RBF_embed(X_train, clusters, opt_sigma)
+    w = lin_reg(Z_train.T, Y_train)
+    Z_test = RBF_embed(X_te, clusters, opt_sigma)
+    err_dual = test_lin_reg(Z_test, Y_te, w)
     print(err_dual)
 
 ####################################################################################################################################
@@ -162,7 +203,7 @@ run_lin_reg(X_tr, Y_tr, X_te, Y_te)
 tr_list = list(range(0, int(X_tr.shape[0]/2)))
 val_list = list(range(int(X_tr.shape[0]/2), X_tr.shape[0]))
 
-# run_dual_reg(X_tr, Y_tr, X_te, Y_te, tr_list, val_list)
+run_dual_reg(X_tr, Y_tr, X_te, Y_te, tr_list, val_list)
 run_non_lin_reg(X_tr, Y_tr, X_te, Y_te, tr_list, val_list)
 
 step_size = 0.01
